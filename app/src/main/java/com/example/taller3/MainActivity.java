14fb2595d1547b4e861c579ba7247313ad07340f
santiagocarod <santiagocaroduque@gmail.com>
2020-11-04 12:54:18 -0500

    Fix Map Activities and added API KEY

--- app/src/main/java/com/example/taller3/MainActivity.java
+++ app/src/main/java/com/example/taller3/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.taller3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.taller3.R;
 
+import cyd.awesome.material.AwesomeButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    AwesomeButton btnLogin;
+    AwesomeButton btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
